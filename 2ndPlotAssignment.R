f2d19c026ac041de1c247c91c2d88052b2c77773
alfonsogd <alfonso.gonzalezdamian@gmail.com>
2014-12-17 12:13:03 -0600

    Primer plot de 2da tarea OK

--- 2ndPlotAssignment.R
+++ 2ndPlotAssignment.R
@@ -1,20 +1,31 @@
 ## This first line will likely take a few seconds. Be patient!
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
-s1 <- NEI[sample(nrow(NEI), 1000, replace = FALSE),]
-#with (NEI, plot(as.factor(year), Emissions, ylim = c(0,100000), main="Emissions by year"))
-with (s1, plot(as.factor(year), Emissions, ylim = c(0,200), main="Emissions by year"))
-#m1  = lm(Emissions ~ year, data = NEI)
-m1  = lm(year ~ Emissions, data = s1)
-#abline(m1, col="blue")
-#abline(m1, col="blue")
-#with (s1, plot(Emissions ~ as.factor(year), type="h"))
+########### Subset para pruebas
+#s1 <- NEI[sample(nrow(NEI), 1000, replace = FALSE),]
+#Emissions <- c(mean(s1[s1$year == 1999,4],trim=0.1),mean(s1[s1$year == 2002,4],trim=0.1),
+#               mean(s1[s1$year == 2005,4],trim=0.1),mean(s1[s1$year == 2008,4],trim=0.1))
+#years <- c(1999,2002,2005,2008)
+#Emybyyear <- data.frame(years, Emissions)
+#modelito <- lm(Emissions ~ years, data = Emybyyear)
+############## Plots para subset
+#par(mfrow =c(1,2), oma = c(0,0,2,0))
+#with(s1,{ plot(year, Emissions, ylim = c(0,200), main="Every fip by year", xlab="Year", ylab="Emissions in tons")
+#          mtext("Emissions by year in the USA", outer = TRUE)})
+#plot (years,Emissions,main="Mean of emissions by year", xlab="year", ylab="Mean in tons",type="l")
+#abline(modelito, col="blue")
+#legend("topright", pch = "-", col =("blue"),legend = ("tendency"))
+########### Base completa
+Emissions <- c(mean(NEI[NEI$year == 1999,4],trim=0.1),mean(NEI[NEI$year == 2002,4],trim=0.1),
+               mean(NEI[NEI$year == 2005,4],trim=0.1),mean(NEI[NEI$year == 2008,4],trim=0.1))
 years <- c(1999,2002,2005,2008)
-Emmissions <- c(sum(s1[s1$year == 1999,4]),sum(s1[s1$year == 2002,4]),
-                sum(s1[s1$year == 2005,4]),sum(s1[s1$year == 2008,4]))
-Emybyyear <- data.frame(years, Emmissions)
-#yylabels <-c(1999,2002,2005,2008)
-#valemiss <- c(sum(NEI[NEI$year == 1999,4]),sum(NEI[NEI$year == 2002,4]),
-#              sum(NEI[NEI$year == 2005,4]),sum(NEI[NEI$year == 2008,4]))
-#plot (yylabels,valemiss,main="Emissions in the USA", xlab="year", ylab="Emissions in tons",type="l")
-plot (years,Emmissions,main="Emissions in the USA", xlab="year", ylab="Emissions in tons",type="l")
+Emybyyear <- data.frame(years, Emissions)
+modelito <- lm(Emissions ~ years, data = Emybyyear)
+########### Plots para base completa
+par(mfrow =c(1,2), oma = c(0,0,2,0))
+with(NEI,{ plot(year, Emissions, ylim = c(0,100000), main="Every fip by year", xlab="Year", ylab="Emissions in tons")
+          mtext("Emissions by year in the USA", outer = TRUE)})
+plot (years,Emissions,main="Mean of emissions by year", xlab="year", ylab="Mean in tons",type="l")
+abline(modelito, col="blue")
+legend("topright", pch = "-", col =("blue"),legend = ("tendency"))
+######### FIN
